8a85162272b36660775921fd05172bdf8c51d7f0
David Bernheisel <david@bernheisel.com>
2015-09-13 02:00:05 -0400

    Add Employee Raise Tests. Add mock reviews

--- employee.rb
+++ employee.rb
@@ -17,24 +17,19 @@ class Employee
     @reviews = reviews
     @email = email
     @phone = phone
+    reviews.each { |r| self.add_review(r) }
   end
 
-  def change_salary(percent: 0, amount: 0)
-    raise ArgumentError "Need a percent OR amount" if percent == 0 && amount == 0
-    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0 && amount != 0
-
-    if percent != 0
-      @salary = @salary * percent+1 if percent > 0
-      @salary = @salary * percent-1 if percent < 0
-    elseif amount != 0
-      @salary += amount
-    end
-
+  def change_salary(percent: 0, adjust_amount: 0)
+    raise ArgumentError "Need a percent OR amount" if percent == 0 && adjust_amount == 0
+    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0 && adjust_amount != 0
+    percent != 0 ?  @salary = @salary + (@salary * percent) : @salary += adjust_amount
     @salary
   end
 
   def add_review(review)
     @reviews << review
+    @performance = review.rating
   end
 
 
