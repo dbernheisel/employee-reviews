6875c6199427dabaa40c6f2fb8c22073b1ffd0fb
David Bernheisel <david@bernheisel.com>
2015-09-13 02:50:09 -0400

    Fix test comparisons. Adjust percent raise logic.

--- department.rb
+++ department.rb
@@ -16,35 +16,20 @@ class Department
     employees.reduce(0) { |sum, e| sum += e.salary }
   end
 
-  def change_salary(percent: 0, adjust_amount: 0, all: false)
-    raise ArgumentError "Need a percent or amount" if percent == 0 && adjust_amount == 0
-    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0 && adjust_amount != 0
+  def change_salary(percent: 0.0, adjust_amount: 0.0, all: false )
+    raise ArgumentError "Need a percent or amount" if percent == 0.0 && adjust_amount == 0.0
+    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0.0 && adjust_amount != 0.0
 
-    unless all
-      good_employees = @employees.select { |e| e.performance > 0.50 }
+    employees = @employees
+    employees = @employees.select { |e| e.performance > 0.50 } unless all
 
-      unless adjust_amount == 0
-        amount_each = adjust_amount / good_employees.length
-        good_employees.each { |e| e.change_salary(adjust_amount: amount_each) }
-      end
-
-      unless percent == 0
-        amount = self.total_salary(good_employees) * (percent / 100)
-        amount_each = amount / good_employees.length
-        good_employees.each { |e| e.change_salary(adjust_amount: amount_each) }
-      end
-    else
-      unless adjust_amount == 0
-        amount_each = adjust_amount / @employees.length
-        @employees.each { |e| e.change_salary(adjust_amount: amount_each) }
-      end
-
-      unless percent == 0
-        amount = self.total_salary * (percent / 100)
-        amount_each = amount / @employees.length
-        @employees.each { |e| e.change_salary(adjust_amount: amount_each) }
-      end
+    unless adjust_amount == 0.0
+      amount_each = adjust_amount / employees.length
+      employees.each { |e| e.change_salary(adjust_amount: amount_each) }
     end
+
+    employees.each { |e| e.change_salary(percent: percent) } unless percent == 0.0
+
   end
 
 end
