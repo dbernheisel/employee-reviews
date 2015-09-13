4b6e97cd1dfde2c10ceed3cb6f4e544c5a31f1ae
David Bernheisel <david@bernheisel.com>
2015-09-12 23:34:07 -0400

    Fix circular references

--- department.rb
+++ department.rb
@@ -1,16 +1,23 @@
 class Department
 
   attr_accessor :name
+  attr_reader :employees
 
-  def initialize(name: name)
+  def initialize(name:, employees: [])
     @name = name
+    @employees = employees
+  end
+
+  def add_employee(employee)
+    @employees << employee
   end
 
   def total_salary
-
+    @employees.reduce(0) { |sum, e| sum += e.salary }
   end
 
-  def raise_salary(percent: percent = 0, amount: amount = 0)
+  def change_salary(percent: 0, amount: 0, all: false)
+    raise ArgumentError "Need a percent or amount" if percent == 0 && amount == 0
 
   end
 
