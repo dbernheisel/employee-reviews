e6f110c9fc6fd3fae9df90d3e0b34070309b6f20
David Bernheisel <david@bernheisel.com>
2015-09-13 23:41:18 -0400

    Update models

--- employee.rb
+++ employee.rb
@@ -1,9 +1,11 @@
+require 'byebug'
 class Employee
 
   attr_reader :name
   attr_reader :salary
   attr_reader :reviews
   attr_reader :performance
+  attr_reader :sentiment_avg
   attr_reader :email
   attr_reader :phone
 
@@ -17,19 +19,25 @@ class Employee
     @reviews = reviews
     @email = email
     @phone = phone
+    @sentiment_avg = 0
     reviews.each { |r| self.add_review(r) }
   end
 
-  def change_salary(percent: 0, adjust_amount: 0)
-    raise ArgumentError "Need a percent OR amount" if percent == 0 && adjust_amount == 0
-    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0 && adjust_amount != 0
-    percent != 0 ?  @salary = @salary + (@salary * percent) : @salary += adjust_amount
-    @salary
+  def change_salary(percent: 0.0, adjust_amount: 0.0)
+    raise ArgumentError "Need a percent OR amount" if percent == 0.0 && adjust_amount == 0.0
+    raise ArgumentError "Provided both arguments, but can only take one" if percent != 0.0 && adjust_amount != 0.0
+    unless percent == 0.0
+      @salary += (@salary * (percent / 100.0))
+    else
+      @salary += adjust_amount
+    end
   end
 
   def add_review(review)
     @reviews << review
-    @performance = review.rating
+    @performance = review.sentiment_rating
+    sentiments = @reviews.collect { |r| r.sentiment_rating }
+    @sentiment_avg = sentiments.reduce{ |sum, r| sum + r }.to_f / @reviews.length
   end
 
 
